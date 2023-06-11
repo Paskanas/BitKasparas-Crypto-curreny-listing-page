714f14b616f13bf051d319b41dcbd82396f38659
Paskanas <kaspe1992@gmail.com>
2023-06-11 17:40:04 +0300

    added logo for light theme

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+const theme = {
+    mode: "light",
+};
+
+if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+    theme.mode = "dark";
+} else {
+    theme.mode = "light";
+}
+</script>
+
 <template>
-    <img src="/images/logo.png" alt="Logo" class="w-1/4" />
+    <img
+        v-if="theme.mode === 'dark'"
+        src="/images/logo.png"
+        alt="Logo"
+        class="w-1/4"
+    />
+    <img v-else src="/images/logoLightS.png" alt="Logo" class="w-1/4" />
 </template>
